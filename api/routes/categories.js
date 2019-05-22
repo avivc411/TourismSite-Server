2d818dbe2cad813b0483178f3802c7d3144909bd
saarmo <asdf>
2019-05-22 22:29:59 +0300

    viewers

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -55,10 +55,11 @@ router.get('/getAllInCategory/:category', (req, res)=>{
 
 router.get('/private/getPopular', (req, res)=> {
     DButilsAzure.execQuery(
-        "select p.[name],Max(p.numOfViewers) as MaxNumOfViewers from (select p.category,MAX(p.numOfViewers) as MaxViewer from userCategories uc join points p on" +
-        " uc.category=p.category where uc.[user]='"+req.decoded.username+"' group by p.category) tmp,points p " +
-        "where tmp.category = p.category and tmp.MaxViewer = p.numOfViewers" +
-        " group by p.[name]")
+        "DECLARE @Category1 VARCHAR(20) DECLARE @Category2 VARCHAR(20) SET @Category1 = (select TOP(1) category from userCategories where userCategories.[user]='"+req.decoded.username+"' order by category asc)" +
+        " SET @Category2 = (select TOP(1) category from userCategories where userCategories.[user]='"+req.decoded.username+"' order by category desc)" +
+        "select TOP (1) [name],[desc],[rank],[numOfViewers],[category] from points where category = @Category1 and [numOfViewers] = (select Max([numOfViewers]) from points where category = @Category1)" +
+        " UNION ALL  select TOP (1) [name],[desc],[rank],[numOfViewers],[category] from points where category = @Category2 " +
+        " and [numOfViewers] = (select Max([numOfViewers]) from points where category = @Category2)")
             .then(function(result){
             res.status(200).send(result);
         })
