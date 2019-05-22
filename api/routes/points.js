7df8ea084f23a521f562d66d9e7e14d3ecd9dc93
avivj <a>
2019-05-22 18:28:32 +0300

    rank point

--- api/routes/points.js
+++ api/routes/points.js
@@ -33,8 +33,8 @@ router.get('/getAllPoints', (req, res)=>{
 });
 
 
-router.get('/getPoint/:pointname', (req, res)=>{
-    const pointName = req.params.pointname;
+router.get('/getPoint/:pointName', (req, res)=>{
+    const pointName = req.params.pointName;
     DButilsAzure.execQuery(
         "SELECT * FROM points where [name]='"+pointName+"';")
         .then(function(result){
@@ -48,14 +48,33 @@ router.get('/getPoint/:pointname', (req, res)=>{
         });
 });
 
+router.post('/private/rankPoint', (req, res, next)=>{
+    DButilsAzure.execQuery(
+        "SELECT * FROM rankedPoints where [user]='"+req.body.username+"' and point='"+req.body.pointName+"'")
+        .then(function(result){
+            if(!result.length===0)
+                res.status(201).json({
+                    message: 'You have already rank this points'
+                });
+            else next();
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+});
 
-
-
-
-
-
-
-
-
+router.post('/private/rankPoint', (req, res)=> {
+    DButilsAzure.execQuery(
+        "insert into rankedPoints values('"+req.body.username+"','"+req.body.pointName+"',"+req.body.rank+")")
+        .then(function(){
+            res.status(200);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    res.send('ok');
+});
 
 module.exports = router;
