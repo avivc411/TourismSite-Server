8cf2e7b6dcf4bd248b1ffeefd69f80ca74f8c882
avivj <a>
2019-05-22 14:45:01 +0300

    categories points

--- api/routes/points.js
+++ api/routes/points.js
@@ -17,4 +17,30 @@ router.get('/getAllPoints', (req, res)=>{
 });
 
 
+
+router.get('/getPoint/:pointname', (req, res)=>{
+    const pointName = req.params.pointname;
+    DButilsAzure.execQuery(
+        "SELECT * FROM points where [name]='"+pointName+"';")
+        .then(function(result){
+            res.status(200).json({
+                points:result
+            });
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+});
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
